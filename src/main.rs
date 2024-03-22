59c5bec4bbd5990a5143135da8ef34c7b5096198
Eric Vogt <eric.vogt.pro@gmail.com>
2024-03-22 18:27:36 +0100

    extract query for hello world

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,28 @@
 use askama_axum::Template;
-use axum::{routing::get, Router};
+use axum::{extract::Query, routing::get, Router};
+use serde::Deserialize;
 
+#[shuttle_runtime::main]
+async fn main() -> shuttle_axum::ShuttleAxum {
+    let router = Router::new().route("/hello", get(hello_world));
+
+    Ok(router.into())
+}
+
+// Hello world handler
 #[derive(Template)]
 #[template(path = "hello.html")]
 struct HelloTemplate {
     name: String,
 }
 
-async fn hello_world() -> HelloTemplate {
-    HelloTemplate { name: "world".to_string() }
+#[derive(Deserialize)]
+pub struct HelloQuery {
+	pub name: String,
 }
 
-#[shuttle_runtime::main]
-async fn main() -> shuttle_axum::ShuttleAxum {
-    let router = Router::new().route("/", get(hello_world));
-
-    Ok(router.into())
+async fn hello_world(Query(params): Query<HelloQuery>) -> HelloTemplate {
+    HelloTemplate {
+        name: params.name,
+    }
 }
