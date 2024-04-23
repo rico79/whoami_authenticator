c8c28d389f627fc076d2c3768fb6435146a6a8aa
Eric Vogt <eric.vogt.pro@gmail.com>
2024-04-23 19:26:26 +0200

    clean signup

--- src/auth/signup.rs
+++ src/auth/signup.rs
@@ -5,20 +5,11 @@ use axum::{
     Form,
 };
 use serde::Deserialize;
-use sqlx::{types::Uuid, Row};
-use tracing::error;
 
-use crate::{utils::crypto::encrypt_text, users::confirm::send_confirmation_email, AppState};
-
-/// Singup errors
-/// List of the different errors that can occur during the signup process
-#[derive(Debug, Deserialize)]
-pub enum Error {
-    DatabaseError,
-    CryptoError,
-    InvalidData,
-    AlreadyExistingUser,
-}
+use crate::{
+    users::{confirm::send_confirmation_email, create_user, UserError},
+    AppState,
+};
 
 /// Template
 /// HTML page definition with dynamic data
@@ -30,34 +21,52 @@ pub struct PageTemplate {
     error: String,
 }
 
+impl PageTemplate {
+    /// Generate page from data
+    pub fn from(
+        name: Option<String>,
+        email: Option<String>,
+        error: Option<UserError>,
+    ) -> PageTemplate {
+        // Prepare error message
+        let error_message = match error {
+            None => "".to_owned(),
+            Some(UserError::AlreadyExistingUser) => format!(
+                "Le mail {} est déjà utilisé",
+                email.clone().unwrap_or("".to_owned())
+            ),
+            Some(UserError::InvalidData) => {
+                "Veuillez corriger les informations remplies".to_owned()
+            }
+            _ => "Un problème est survenu, veuillez réessayer plus tard".to_owned(),
+        };
+
+        PageTemplate {
+            error: error_message,
+            name: name.unwrap_or("".to_owned()),
+            email: email.unwrap_or("".to_owned()),
+        }
+    }
+
+    /// Generate page from query params
+    pub fn from_query(params: QueryParams) -> PageTemplate {
+        Self::from(params.name, params.email, params.error)
+    }
+}
+
 /// Query parameters definition
 /// HTTP parameters used for the get Handler
 #[derive(Deserialize)]
 pub struct QueryParams {
     name: Option<String>,
     email: Option<String>,
-    error: Option<Error>,
+    error: Option<UserError>,
 }
 
 /// Get handler
 /// Returns the page using the dedicated HTML template
 pub async fn get(Query(params): Query<QueryParams>) -> impl IntoResponse {
-    // Prepare error message
-    let error_message = match params.error {
-        None => "".to_owned(),
-        Some(Error::AlreadyExistingUser) => format!(
-            "Le mail {} est déjà utilisé",
-            params.email.clone().unwrap_or("".to_owned())
-        ),
-        Some(Error::InvalidData) => "Veuillez corriger les informations remplies".to_owned(),
-        _ => "Un problème est survenu, veuillez réessayer plus tard".to_owned(),
-    };
-
-    PageTemplate {
-        error: error_message,
-        name: params.name.unwrap_or("".to_owned()),
-        email: params.email.unwrap_or("".to_owned()),
-    }
+    PageTemplate::from_query(params)
 }
 
 /// Signup form
@@ -75,102 +84,27 @@ pub struct SignupForm {
 pub async fn post(
     State(state): State<AppState>,
     Form(form): Form<SignupForm>,
-) -> impl IntoResponse {
-    // Check if data are correct
-    if !&form.name.is_empty()
-        && !&form.email.is_empty()
-        && !&form.password.is_empty()
-        && !&form.confirm_password.is_empty()
-        && &form.password == &form.confirm_password
-    {
-        // Encrypt the password
-        match encrypt_text(&form.password) {
-            Ok(encrypted_password) => {
-                // Insert the user
-                let query_result = sqlx::query(
-                    "INSERT INTO users (name, email, encrypted_password) VALUES ($1, $2, $3) RETURNING user_id",
-                )
-                .bind(&form.name)
-                .bind(&form.email)
-                .bind(encrypted_password)
-                .fetch_one(&state.db_pool)
-                .await;
+) -> Result<impl IntoResponse, PageTemplate> {
+    // Create user and get user_id generated
+    let user_id = create_user(
+        &state,
+        &form.name,
+        &form.email,
+        &form.password,
+        &form.confirm_password,
+    )
+    .await
+    .map_err(|error| {
+        PageTemplate::from(
+            Some(form.name.clone()),
+            Some(form.email.clone()),
+            Some(error),
+        )
+    })?;
 
-                // Check the result
-                match query_result {
-                    Ok(row) => {
-                        // Get the user id created
-                        let user_id = row.get::<Uuid, &str>("user_id");
+    // Send confirmation email
+    send_confirmation_email(&state, &form.name, &form.email, &user_id);
 
-                        // Send the email for validation
-                        send_confirmation_email(
-                            &state,
-                            &form.name,
-                            &form.email,
-                            &user_id.to_string(),
-                        );
-
-                        // Redirect to hello page
-                        Redirect::to(&format!("/hello?message={}", form.name))
-                    }
-                    Err(sqlx::Error::Database(database_error)) => {
-                        // If it is a unique violation error, it means that the user mail already existed
-                        if database_error.is_unique_violation() {
-                            Redirect::to(&format!(
-                                "/signup?error={:?}&name={}&email={}",
-                                Error::AlreadyExistingUser,
-                                &form.name,
-                                &form.email
-                            ))
-                        } else {
-                            error!(
-                                "Signup impossible for user {} due to db error: '{}'",
-                                &form.email, database_error
-                            );
-
-                            Redirect::to(&format!(
-                                "/signup?error={:?}&name={}&email={}",
-                                Error::DatabaseError,
-                                &form.name,
-                                &form.email
-                            ))
-                        }
-                    }
-                    Err(error) => {
-                        error!(
-                            "Signup impossible for user {} due to db error: '{}'",
-                            &form.email, error
-                        );
-
-                        Redirect::to(&format!(
-                            "/signup?error={:?}&name={}&email={}",
-                            Error::DatabaseError,
-                            &form.name,
-                            &form.email
-                        ))
-                    }
-                }
-            }
-            Err(error) => {
-                error!(
-                    "Signup impossible for user {} due to crypto error: '{}'",
-                    &form.email, error
-                );
-
-                Redirect::to(&format!(
-                    "/signup?error={:?}&name={}&email={}",
-                    Error::CryptoError,
-                    &form.name,
-                    &form.email
-                ))
-            }
-        }
-    } else {
-        Redirect::to(&format!(
-            "/signup?error={:?}&name={}&email={}",
-            Error::InvalidData,
-            &form.name,
-            &form.email
-        ))
-    }
+    // Connect the user and redirect
+    Ok(Redirect::to("/hello"))
 }
--- src/users.rs
+++ src/users.rs
@@ -1,1 +1,63 @@
+use serde::Deserialize;
+use sqlx::{types::Uuid, Row};
+
+use crate::{utils::crypto::encrypt_text, AppState};
+
 pub mod confirm;
+
+/// Error types for auth errors
+#[derive(Debug, Deserialize)]
+pub enum UserError {
+    DatabaseError,
+    CryptoError,
+    InvalidData,
+    AlreadyExistingUser,
+}
+
+/// Create User from signup
+/// Use the cookies and the App state
+/// Get name, email, password and password confirmation
+/// Return user_id
+pub async fn create_user(
+    state: &AppState,
+    name: &String,
+    email: &String,
+    password: &String,
+    confirm_password: &String,
+) -> Result<String, UserError> {
+    // Check if missing data and if password does not match confirmation
+    if name.is_empty()
+        || email.is_empty()
+        || password.is_empty()
+        || confirm_password.is_empty()
+        || password != confirm_password
+    {
+        return Err(UserError::InvalidData);
+    }
+
+    // Encrypt the password
+    let encrypted_password = encrypt_text(password).map_err(|_| UserError::CryptoError)?;
+
+    // Insert the user
+    let row = sqlx::query(
+        "INSERT INTO users (name, email, encrypted_password) VALUES ($1, $2, $3) RETURNING user_id",
+    )
+    .bind(name)
+    .bind(email)
+    .bind(encrypted_password)
+    .fetch_one(&state.db_pool)
+    .await
+    .map_err(|error| match error {
+        sqlx::Error::Database(error) => {
+            if error.is_unique_violation() {
+                UserError::AlreadyExistingUser
+            } else {
+                UserError::DatabaseError
+            }
+        }
+        _ => UserError::DatabaseError,
+    })?;
+
+    // Return user_id
+    Ok(row.get::<Uuid, &str>("user_id").to_string())
+}
