e689d6d62cf78140c7bfe97da11594ca7d2ebd88
Eric Vogt <eric.vogt.pro@gmail.com>
2024-04-28 14:24:48 +0200

    on delete app foreign key on users

--- migrations/20240412071811_db_scheme.sql
+++ migrations/20240412071811_db_scheme.sql
@@ -20,7 +20,7 @@ CREATE TABLE IF NOT EXISTS apps (
     jwt_secret VARCHAR,
     jwt_seconds_to_expire INTEGER,
     created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
-    owner_id INTEGER REFERENCES users(id)
+    owner_id INTEGER REFERENCES users ON DELETE SET NULL
 );
 
 -- Sessions
