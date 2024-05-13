1f166974e7076cc5c77824eb7ac5ed4b540d6e85
Eric Vogt <eric.vogt.pro@gmail.com>
2024-05-13 11:46:56 +0200

    absolute path for styles and js libraries

--- src/main.rs
+++ src/main.rs
@@ -91,7 +91,7 @@ async fn http_server(
             "/app",
             get(apps::app::get_handler).post(apps::app::post_handler),
         )
-        .route("/authorize", get(open_id::authorize_handler))
+        .route("/openid/authorize", get(open_id::authorize_handler))
         .nest_service("/assets", ServeDir::new("assets"))
         .nest_service("/favicon.ico", ServeFile::new("assets/favicon.ico"))
         .with_state(state);
